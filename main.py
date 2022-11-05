2413336a227cc0db6021b0e88f524a56be323d1d
Davi Belo <davibelo@gmail.com>
2022-11-05 09:37:53 -0300

    env variables and db to postGres

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 from datetime import date
+import os
 
 app = Flask(
     __name__#,instance_path="C:\\Users\\davib\\Desktop\\blog"
 )
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
@@ -44,7 +45,8 @@ def admin_only(f):
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
